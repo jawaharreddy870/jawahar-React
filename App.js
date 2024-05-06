7ffb55b58c78f7b4695a7dde9235a844e3ad19e5
jawaharreddy870 <jawaharreddy870@example.com>
2024-05-06 22:16:50 +0530

    added data

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h1',{id:'Jawahar',className:"jawaclass"},'Hello world from app');
 // console.log(heading);
-const heading1 = React.createElement('h1',{id:'Jawahar',className:"jawaclass"},React.createElement('h2',{id:'Jawahar2',className:"jawaclass3"},'Heading 2'));
+const heading1 = React.createElement('h1',{id:'Jawahar',className:"jawaclass"},React.createElement('h2',{id:'Jawahar2',className:"jawaclass3"},'Heading 2 y'));
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading1)
