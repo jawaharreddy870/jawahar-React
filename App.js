2a7e56d6fa0f1c0b087865781b708aac6585b8c7
jawaharreddy870 <jawaharreddy870@example.com>
2024-06-25 21:34:53 +0530

    added episode 4

--- App.js
+++ App.js
@@ -1,8 +1,535 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement('h1',{id:'Jawahar',className:"jawaclass"},'Hello world from app');
-// console.log(heading);
-const heading1 = React.createElement('h1',{id:'Jawahar',className:"jawaclass"},React.createElement('h2',{id:'Jawahar2',className:"jawaclass3"},'Heading 2 y'));
-let root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading1)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li> Home </li>
+          <li> About </li>
+          <li> Contact Us </li>
+          <li> Cart </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const resCards = [
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "251194",
+          name: "Chickpet donne biriyani house",
+          cloudinaryImageId:
+            "FOOD_CATALOG/IMAGES/CMS/2024/4/22/81d1977f-5b5e-4391-9ed8-14b32008c8f0_4f5cd3a7-4214-4488-8ccc-db1940458e53.jpeg",
+          locality: "Koramangala",
+          areaName: "Koramangala",
+          costForTwo: "₹250 for two",
+          cuisines: ["Biryani"],
+          avgRating: 4.1,
+          parentId: "243696",
+          avgRatingString: "4.1",
+          totalRatingsString: "5K+",
+          promoted: true,
+          adTrackingId:
+            "cid=14012362~p=0~adgrpid=14012362#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=251194~eid=5bf9d624-0322-4bb4-b147-abbed1c04872~srvts=1719329922218~collid=83639",
+          sla: {
+            deliveryTime: 37,
+            lastMileTravel: 5.6,
+            serviceability: "SERVICEABLE",
+            slaString: "35-40 mins",
+            lastMileTravelString: "5.6 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-06-25 23:59:00",
+            opened: true,
+          },
+          badges: {
+            textExtendedBadges: [
+              {
+                iconId: "guiltfree/GF_Logo_android_3x",
+                shortDescription: "options available",
+                fontColor: "#7E808C",
+              },
+            ],
+          },
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {
+                badgeObject: [
+                  {
+                    attributes: {
+                      fontColor: "#7E808C",
+                      iconId: "guiltfree/GF_Logo_android_3x",
+                      description: "",
+                      shortDescription: "options available",
+                    },
+                  },
+                ],
+              },
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "20% OFF",
+            subHeader: "UPTO ₹50",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "3.8",
+              ratingCount: "1K+",
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=251194&source=collection&query=Biryani",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "39063",
+          name: "Hotel Navayuga",
+          cloudinaryImageId: "yr4hhv0mjiolv46oju8l",
+          locality: "Majestic",
+          areaName: "Majestic",
+          costForTwo: "₹400 for two",
+          cuisines: ["Andhra"],
+          avgRating: 4.4,
+          parentId: "21555",
+          avgRatingString: "4.4",
+          totalRatingsString: "5K+",
+          sla: {
+            deliveryTime: 27,
+            lastMileTravel: 2.8,
+            serviceability: "SERVICEABLE",
+            slaString: "25-30 mins",
+            lastMileTravelString: "2.8 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-06-25 22:00:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          aggregatedDiscountInfoV2: {},
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "4.2",
+              ratingCount: "5K+",
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=39063&source=collection&query=Biryani",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "270",
+          name: "Donne Biryani House",
+          cloudinaryImageId:
+            "FOOD_CATALOG/IMAGES/CMS/2024/4/22/77f55014-fab9-4018-b971-f84e33054b05_2bd44604-a472-4c32-8c9c-d77d3f73163b.jpeg",
+          locality: "7th Block",
+          areaName: "Koramangala",
+          costForTwo: "₹130 for two",
+          cuisines: ["Biryani"],
+          avgRating: 4.2,
+          parentId: "338",
+          avgRatingString: "4.2",
+          totalRatingsString: "10K+",
+          promoted: true,
+          adTrackingId:
+            "cid=14012352~p=1~adgrpid=14012352#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=270~eid=1e3237d4-aa7a-439f-8add-7667330a11f7~srvts=1719329922218~collid=83639",
+          sla: {
+            deliveryTime: 34,
+            lastMileTravel: 5,
+            serviceability: "SERVICEABLE",
+            slaString: "30-35 mins",
+            lastMileTravelString: "5.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-06-25 23:59:00",
+            opened: true,
+          },
+          badges: {
+            textExtendedBadges: [
+              {
+                iconId: "guiltfree/GF_Logo_android_3x",
+                shortDescription: "options available",
+                fontColor: "#7E808C",
+              },
+            ],
+          },
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textExtendedBadges: {
+                badgeObject: [
+                  {
+                    attributes: {
+                      iconId: "guiltfree/GF_Logo_android_3x",
+                      description: "",
+                      shortDescription: "options available",
+                      fontColor: "#7E808C",
+                    },
+                  },
+                ],
+              },
+              textBased: {},
+              imageBased: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "20% OFF",
+            subHeader: "UPTO ₹50",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=270&source=collection&query=Biryani",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "291533",
+          name: "New Ambur Biryani",
+          cloudinaryImageId: "d7e637e0db8abc11749c6faa7d54af3f",
+          locality: "Sudhama Nagar",
+          areaName: "AUSTIN TOWN",
+          costForTwo: "₹300 for two",
+          cuisines: ["Biryani"],
+          avgRating: 4.1,
+          parentId: "145761",
+          avgRatingString: "4.1",
+          totalRatingsString: "100+",
+          sla: {
+            deliveryTime: 26,
+            lastMileTravel: 2.1,
+            serviceability: "SERVICEABLE",
+            slaString: "25-30 mins",
+            lastMileTravelString: "2.1 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-06-25 22:30:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          aggregatedDiscountInfoV3: {
+            header: "20% OFF",
+            subHeader: "UPTO ₹50",
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "--",
+            },
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=291533&source=collection&query=Biryani",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+  {
+    card: {
+      card: {
+        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        info: {
+          id: "122795",
+          name: "Mayura 1989",
+          cloudinaryImageId: "3b8e514328e772c9e7d8449c53d860b3",
+          locality: "Rajajinagar",
+          areaName: "Basaveshwara Nagar",
+          costForTwo: "₹900 for two",
+          cuisines: [
+            "Chinese",
+            "South Indian",
+            "North Indian",
+            "Desserts",
+            "Fast Food",
+            "Andhra",
+            "Mughlai",
+            "Seafood",
+            "Punjabi",
+            "Hyderabadi",
+          ],
+          avgRating: 4.2,
+          parentId: "134014",
+          avgRatingString: "4.2",
+          totalRatingsString: "1K+",
+          promoted: true,
+          adTrackingId:
+            "cid=14706080~p=3~adgrpid=14706080#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=122795~eid=5b697f29-cfe1-46f1-ab6d-16c25a076c73~srvts=1719329922218~collid=83639",
+          sla: {
+            deliveryTime: 38,
+            lastMileTravel: 5,
+            serviceability: "SERVICEABLE",
+            slaString: "35-40 mins",
+            lastMileTravelString: "5.0 km",
+            iconType: "ICON_TYPE_EMPTY",
+          },
+          availability: {
+            nextCloseTime: "2024-06-25 23:30:00",
+            opened: true,
+          },
+          badges: {},
+          isOpen: true,
+          aggregatedDiscountInfoV2: {},
+          type: "F",
+          badgesV2: {
+            entityBadges: {
+              textBased: {},
+              imageBased: {},
+              textExtendedBadges: {},
+            },
+          },
+          orderabilityCommunication: {
+            title: {},
+            subTitle: {},
+            message: {},
+            customIcon: {},
+          },
+          differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+              mediaType: "ADS_MEDIA_ENUM_IMAGE",
+              lottie: {},
+              video: {},
+            },
+          },
+          reviewsSummary: {},
+          displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+          restaurantOfferPresentationInfo: {},
+          externalRatings: {
+            aggregatedRating: {
+              rating: "3.9",
+              ratingCount: "5K+",
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+          },
+          ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        },
+        analytics: {},
+        cta: {
+          link: "swiggy://menu?restaurant_id=122795&source=collection&query=Biryani",
+          text: "RESTAURANT_MENU",
+          type: "DEEPLINK",
+        },
+        widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food",
+      },
+      relevance: {
+        type: "RELEVANCE_TYPE_ON_MENU_RETURN",
+        sectionId: "MENU_RETURN_FOOD",
+      },
+    },
+  },
+];
+
+const RestaurentCard = (props) => {
+  const { resData } = props;
+  console.log(resData.card.card.info);
+  const { name, cuisines, avgRating, sla } = resData.card.card.info;
+  const { deliveryTime } = sla;
+
+  console.log(props, "props");
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+      ></img>
+      <h3> {name} </h3>
+      <h4> {cuisines.join(", ")}</h4>
+      <h4> {avgRating}</h4>
+      <h4> {deliveryTime} </h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">search</div>
+      <div className="res-container">
+        {resCards.map((item) => (
+          <RestaurentCard resData={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
